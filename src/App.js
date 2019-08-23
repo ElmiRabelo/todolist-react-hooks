f13b52a2ef6407e04b0e6bde4bd223c036f6415b
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-22 21:05:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./components/todo-list.component";
+
+function App() {
+	return (
+		<div className="App">
+			<TodoList />
+		</div>
+	);
+}
+
+export default App;
